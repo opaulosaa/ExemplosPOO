c3fe8a9f26ac22d01956d44fd687fb852ab509d0
opaulosaa <pauloarneto@gmail.com>
2024-09-23 15:24:46 -0300

    Add files via upload

---
+++ anotations/AnotationEx.java
@@ -0,0 +1,9 @@
+package com.paulosa.anotations;
+@Tabela(
+        nome = "Tabela 1"
+)
+public class AnotationEx {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ anotations/Tabela.java
@@ -0,0 +1,5 @@
+package com.paulosa.anotations;
+
+public @interface Tabela {
+    String nome();
+}
